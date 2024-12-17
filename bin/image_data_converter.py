fc0eef06fe5f1b475025e0cfcdbb1c84313f93df
Tonicx <1254654267@qq.com>
2024-12-17 13:58:43 +0800

    debug

--- bin/image_data_converter.py
+++ bin/image_data_converter.py
@@ -1,32 +1,31 @@
-
 import pickle
 import warnings
 import datetime
-import optparse
-import os, errno
-# from keras.preprocessing.image import array_to_img
-from keras.preprocessing import image
-from keras.applications.vgg16 import preprocess_input
+import argparse  # 使用 argparse 替代 optparse
+import os
+import errno
+import tensorflow as tf  # 使用 TensorFlow 的 Keras 库
+from tensorflow.keras.preprocessing import image  # 更新为 tensorflow.keras.preprocessing
+from tensorflow.keras.applications.vgg16 import preprocess_input  # 更新为 tensorflow.keras.applications
 import numpy as np
 
 
 def preprocess_input_vgg(x):
-    """Wrapper around keras.applications.vgg16.preprocess_input()
-    to make it compatible for use with keras.preprocessing.image.ImageDataGenerator's
+    """Wrapper around tensorflow.keras.applications.vgg16.preprocess_input()
+    to make it compatible for use with tensorflow.keras.preprocessing.image.ImageDataGenerator's
     `preprocessing_function` argument.
 
     Parameters
     ----------
     x : a numpy 3darray (a single image to be preprocessed)
 
-    Note we cannot pass keras.applications.vgg16.preprocess_input()
-    directly to to keras.preprocessing.image.ImageDataGenerator's
+    Note we cannot pass tensorflow.keras.applications.vgg16.preprocess_input()
+    directly to tensorflow.keras.preprocessing.image.ImageDataGenerator's
     `preprocessing_function` argument because the former expects a
     4D tensor whereas the latter expects a 3D tensor. Hence the
     existence of this wrapper.
 
     Returns a numpy 3darray (the preprocessed image).
-
     """
     X = np.expand_dims(x, axis=0)
     X = preprocess_input(X)
@@ -37,26 +36,27 @@ def read_train_data_multimodal(data_file, delim="\t"):
     Prepare the data
     """
     image_list = []
-    with open(data_file, 'rU') as f:
-        #next(f)
+    with open(data_file, 'r') as f:  # 'rU' 在 Python 3 中已废弃，直接使用 'r'
         for line in f:
             line = line.strip()
-            if (line == ""):
+            if line == "":
                 continue
             row = line.split(delim)
-            #txt = row[2].strip()
             image_path = row[0].strip()
             image_list.append(image_path)
 
-    return image_list;
+    return image_list
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore")
-    parser = optparse.OptionParser()
-    parser.add_option('-i', action="store", dest="input_file_name", default=None, type="string")
-    parser.add_option('-o', action="store", dest="output_file_name", default=None, type="string")
 
-    options, args = parser.parse_args()
+    # 使用 argparse 替代 optparse
+    parser = argparse.ArgumentParser(description="Process some images.")
+    parser.add_argument('-i', '--input_file_name', type=str, required=True, help="Input file name")
+    parser.add_argument('-o', '--output_file_name', type=str, required=True, help="Output file name")
+
+    options = parser.parse_args()
+
     a = datetime.datetime.now().replace(microsecond=0)
 
     input_file_name = options.input_file_name
@@ -64,16 +64,13 @@ if __name__ == "__main__":
     data = read_train_data_multimodal(input_file_name)
 
     images_npy_data = {}  # np.empty([len(instances), 224, 224, 3])
-    for i ,img_path in enumerate(data):
-        #img_path = inst.imgpath
+    for i, img_path in enumerate(data):
         img = image.load_img(img_path, target_size=(224, 224))
         img_data = image.img_to_array(img)
         img_data = np.expand_dims(img_data, axis=0)
         img_data = preprocess_input(img_data)
         images_npy_data[img_path] = img_data
 
+    # 将数据保存为 pickle 文件
     with open(output_file_name, 'wb') as handle:
         pickle.dump(images_npy_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    #with open('../filename.pickle', 'rb') as handle:
-    #    images_npy_data = pickle.load(handle)
